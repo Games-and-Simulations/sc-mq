cdd13f24367fa0981c37f10b7c3a9f588a8b5a41
Michal Sustr <michal.sustr@blindspot.ai>
2018-01-18 18:00:16 +0100

    Add setup.py scripts

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+Starcraft BW docker launcher.
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+from tournament import VERSION
+
+setup(
+    name='scbw_mq',
+    version=VERSION,
+    description='Message queues for distributed use of scbw package',
+    long_description="Message queue for starcraft workers."
+                     "Allow spawning of multiple workers that play starcraft games, orchestrated by RabbitMQ."
+                     "See https://github.com/Games-and-Simulations/sc-mq for more information.",
+    url='https://github.com/Games-and-Simulations/sc-mq',
+    author='Michal Sustr',
+    author_email='michal.sustr@aic.fel.cvut.cz',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='starcraft broodwar rabbitmq tournament',
+    install_requires=['pika',
+                      'coloredlogs',
+                      'tqdm',
+                      'scbw'],
+    extras_require={
+    },
+    packages=['tournament'],
+    entry_points={  # Optional
+        'console_scripts': [
+            'scbw.tournament.consume=tournament.cli:consumer',
+            'scbw.tournament.produce=tournament.cli:producer',
+        ],
+    },
+    python_requires='>=3.6',
+)
